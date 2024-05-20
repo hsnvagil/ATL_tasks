79f753144f5e6997cfeb3f3fc688778da1e31cdd
Agil <hsnvagil@gmail.com>
2024-05-20 17:37:32 +0400

    Comparable

--- src/lesson_15/MainApp.java
+++ src/lesson_15/MainApp.java
@@ -24,7 +24,7 @@ public class MainApp {
         Arrays.sort(students, new Comparator<Student>() {
             @Override
             public int compare(Student o1, Student o2) {
-                return o1.name.compareTo(o2.name);
+                return -o1.name.compareTo(o2.name);
             }
         });
 
